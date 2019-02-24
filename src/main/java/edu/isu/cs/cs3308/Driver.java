ecaa6b388c85794d91d925746bc4b0ddc6c61ade
harvaaro <harvaaro@isu.edu>
2019-02-24 02:48:50 -0700

    Driver seems to work now, just need to finish up the documentation.

--- src/main/java/edu/isu/cs/cs3308/Driver.java
+++ src/main/java/edu/isu/cs/cs3308/Driver.java
@@ -20,74 +20,111 @@ import java.util.Random;
  */
 public class Driver {
 
-    public static void main(String args[]) {
-        // do the simulation using generateRandomArray()
+	public static void main(String args[]) {
+		// do the simulation using generateRandomArray()
+        int numRange = 2000;
+        ArraySearch[] searchAlgo = {
+                new LinearSearch()
+                ,new RecursiveLinearSearch()
+                ,new BinarySearch()
+                ,new RecursiveBinarySearch()
+        };
+        int numAlgos = searchAlgo.length;
+        long[] avgTime = new long[numAlgos];
+        long[][] timeTaken = new long[numAlgos][];
+        for (int i = 0; i < numAlgos; i++) {
+            timeTaken[i] = new long[numRange];
+        }
 
-        // report the results using report;
+        for (int set = 0; set < numRange; set++) {
+            Integer[] randNumList = generateRandomArray(numRange);
+            Random numFind = new Random();
+
+            for (int rep = 0; rep < numRange; rep++) {
+                int searchFor = numFind.nextInt(numRange);
+                for (int type = 0; type < numAlgos; type++) {
+                    avgTime[type] = timedSearch(searchAlgo[type],avgTime[type],randNumList,searchFor);
+                }
+            }
+
+            for (int type = 0; type < numAlgos; type++) {
+                timeTaken[type][set] = avgTime[type] / numRange;
+            }
+        }
+
+		// report the results using report;
+        report(timeTaken[0],timeTaken[1],timeTaken[2],timeTaken[3],0,1);
+	}
+
+	private static long timedSearch(ArraySearch searchAlgo, long avgTime, Integer[] randList, int numFind) {
+	    long timeStart = System.nanoTime();
+	    searchAlgo.search(randList,numFind);
+	    long timeEnd = System.nanoTime();
+	    return (avgTime + (timeEnd - timeStart));
     }
 
-    /**
-     * Generates a random ordered array of integers of the provided size
-     *
-     * @param size Size of the random array
-     * @return An array of the provided size of random numbers in ascending
-     * order.
-     */
-    public static Integer[] generateRandomArray(int size) {
-        Random rand = new Random();
+	/**
+	 * Generates a random ordered array of integers of the provided size
+	 *
+	 * @param size Size of the random array
+	 * @return An array of the provided size of random numbers in ascending
+	 * order.
+	 */
+	public static Integer[] generateRandomArray(int size) {
+		Random rand = new Random();
 
-        Integer[] array = new Integer[size];
+		Integer[] array = new Integer[size];
 
-        for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(2000);
-        }
+		for (int i = 0; i < size; i++) {
+			array[i] = rand.nextInt(2000);
+		}
 
-        Arrays.sort(array);
-        return array;
-    }
+		Arrays.sort(array);
+		return array;
+	}
 
-    /**
-     * Generates the output to both a Comma Separated Values file called
-     * "results.csv" and to the screen.
-     *
-     * @param iterLinTimes Array of average values for the Iterative Linear
-     * Search
-     * @param recLinTimes Array of average values for the Recursive Linear
-     * Search
-     * @param iterBinTimes Array of average values for the Iterative Binary
-     * Search
-     * @param recBinTimes Array of average values for the Recursive Binary
-     * Search
-     * @param startIncrement Start increment for array sizes
-     * @param increment Increment of array sizes.
-     */
-    private static void report(long[] iterLinTimes, long[] recLinTimes, long[] iterBinTimes, long[] recBinTimes, int startIncrement, int increment) {
-        StringBuilder file = new StringBuilder();
-        StringBuilder screen = new StringBuilder();
+	/**
+	 * Generates the output to both a Comma Separated Values file called
+	 * "results.csv" and to the screen.
+	 *
+	 * @param iterLinTimes Array of average values for the Iterative Linear
+	 * Search
+	 * @param recLinTimes Array of average values for the Recursive Linear
+	 * Search
+	 * @param iterBinTimes Array of average values for the Iterative Binary
+	 * Search
+	 * @param recBinTimes Array of average values for the Recursive Binary
+	 * Search
+	 * @param startIncrement Start increment for array sizes
+	 * @param increment Increment of array sizes.
+	 */
+	private static void report(long[] iterLinTimes, long[] recLinTimes, long[] iterBinTimes, long[] recBinTimes, int startIncrement, int increment) {
+		StringBuilder file = new StringBuilder();
+		StringBuilder screen = new StringBuilder();
 
-        screen.append(String.format("N    IterLin\tRecLin\tIterBin\tRecBin%s", System.lineSeparator()));
-        file.append(String.format("N,IterLin,RecLin,IterBin,RecBin%s", System.lineSeparator()));
+		screen.append(String.format("N    IterLin\tRecLin\tIterBin\tRecBin%s", System.lineSeparator()));
+		file.append(String.format("N,IterLin,RecLin,IterBin,RecBin%s", System.lineSeparator()));
 
-        for (int i = 0; i < iterLinTimes.length; i++) {
-            screen.append(String.format("%d %d\t%d\t%d\t%d%s", startIncrement + (i * increment), iterLinTimes[i], recLinTimes[i], iterBinTimes[i], recBinTimes[i], System.lineSeparator()
-            ));
-            file.append(String.format("%d,%d,%d,%d,%d%s", startIncrement + (i * increment), iterLinTimes[i], recLinTimes[i], iterBinTimes[i], recBinTimes[i], System.lineSeparator()
-            ));
-        }
+		for (int i = 0; i < iterLinTimes.length; i++) {
+			screen.append(String.format("%d %d\t%d\t%d\t%d%s", startIncrement + (i * increment), iterLinTimes[i], recLinTimes[i], iterBinTimes[i], recBinTimes[i], System.lineSeparator()
+			));
+			file.append(String.format("%d,%d,%d,%d,%d%s", startIncrement + (i * increment), iterLinTimes[i], recLinTimes[i], iterBinTimes[i], recBinTimes[i], System.lineSeparator()
+			));
+		}
 
-        System.out.println(screen.toString());
+		System.out.println(screen.toString());
 
-        Path p = Paths.get("results.csv");
-        try {
-            Files.deleteIfExists(p);
-        } catch (IOException e) {
+		Path p = Paths.get("results.csv");
+		try {
+			Files.deleteIfExists(p);
+		} catch (IOException e) {
 
-        }
+		}
 
-        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(p, StandardOpenOption.CREATE, StandardOpenOption.WRITE))) {
-            pw.println(file.toString());
-        } catch (IOException e) {
+		try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(p, StandardOpenOption.CREATE, StandardOpenOption.WRITE))) {
+			pw.println(file.toString());
+		} catch (IOException e) {
 
-        }
-    }
+		}
+	}
 }
